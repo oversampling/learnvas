d7a59bd0a1089c1b816bbe2e3e1f13610614efa4
chan1992241 <chan1992241@gmail.com>
2022-06-08 04:49:30 +0800

    Create port for deployment

--- index.js
+++ index.js
@@ -190,6 +190,8 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
 });

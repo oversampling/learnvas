40b7a4a2cdcaab395580037d3cd7ae329fe9a9f1
chan1992241 <chan1992241@gmail.com>
2022-06-08 00:50:53 +0800

    fix mongodb uri to .env

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect("mongodb://localhost:27017/tuition-app", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
